00eebdec92e4b847004bf986737a98ee40cd7896
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-13 17:06:29 +0530

    Add header with game logo and title; implement Player component for player management

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+export default function Player({initialName, symbol}) {
+
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+
+    function handleEditButton(){
+        setIsEditing((Editing)=>!Editing);
+    }
+
+    function handleNameChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let content = <span className="player-name">{playerName}</span>;
+
+    if(isEditing){
+        content = <input type="text" required value={playerName} onChange={handleNameChange}/>;
+    }
+    return(
+        <li>
+            <span className="player">
+                {content}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditButton}>{isEditing?"Save":"Edit"}</button>
+        </li>
+    );
+}
