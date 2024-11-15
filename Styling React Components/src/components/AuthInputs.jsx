9ee1dae4e57302b2cece984d3fee24d10a63b14a
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-15 14:14:41 +0530

    Refactor AuthInputs component to use new Input component; remove CustomInputs and update Button styling

--- Styling React Components/src/components/AuthInputs.jsx
+++ Styling React Components/src/components/AuthInputs.jsx
@@ -1,18 +1,6 @@
 import { useState } from 'react';
-import {styled} from 'styled-components';
 import Button from './Button';
-import CustomInputs from './CustomInputs';
-
-const ControlDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-
-
-
+import Input from './Input';
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -35,24 +23,24 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDiv>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <p>
-          <CustomInputs $invalid = {emailNotValid} type="email" onChange={(event) =>
+          <Input invalid = {emailNotValid} label={"Email"} type="email" onChange={(event) =>
               handleInputChange('email', event.target.value)
-            } >Email</CustomInputs>
+            } />
         </p>
         <p>
-        <CustomInputs $invalid = {passwordNotValid} type="email" onChange={(event) =>
+        <Input invalid = {passwordNotValid} label={"Password"} type="email" onChange={(event) =>
               handleInputChange('password', event.target.value)
-            } >Password</CustomInputs>
+            } />
         </p>
-      </ControlDiv>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button  onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
