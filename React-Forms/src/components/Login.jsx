7c8f89d9ef79a829aae26a89caa4dcacb0f9d549
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-20 17:52:12 +0530

    Refactor Login component to include StateLogin and RefLogin; implement email validation and state management for form inputs.

--- React-Forms/src/components/Login.jsx
+++ React-Forms/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 //REF LOGIC
 export default function Login() {
+
+  const [emailInvalid, setEmailInvalid] = useState(false);
 
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -8,18 +10,26 @@ export default function Login() {
   function handleSubmit(event) {
     event.preventDefault();
     console.log('Login form submitted \nEmail:'+ emailRef.current.value + '\nPassword: ' + passwordRef.current.value);
-
+    const emailIsValid = emailRef.current.value.includes('@');
+    if(!emailIsValid){
+      setEmailInvalid(true);
+      return;
+    }
+    setEmailInvalid(false);
   }
 
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+      <h2>RefLogin</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={emailRef} />
+          <div className="control-error">
+            {emailInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
--- React-Forms/src/components/StateLogin.jsx
+++ React-Forms/src/components/StateLogin.jsx
@@ -9,34 +9,57 @@ export default function Login() {
     password: ''
   });
 
+  const [didEdit,setDidEdit] = useState({
+    email:false,
+    password:false
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredData.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log('Login form submitted \nData: ');
     console.log(enteredData);
+    if(emailIsInvalid){
+      return;
+    }
 
   }
 
 
   function handleInputChange(identifier, value) {
-    setEnteredData((prevData) => {
-      return {
+    setEnteredData((prevData) => ({
         ...prevData,
         [identifier]: value
-      };
-    });
+    }));
+    setDidEdit((prevData)=>({
+      ...prevData,
+      [identifier]:false
+    }));
+  }
+
+  function handleInputBlur(identifier){
+    setDidEdit((prevData)=>({
+      ...prevData,
+      [identifier]:true
+    }));
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+      <h2>StateLogin</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" 
+          <input id="email"  name="email" 
+            onBlur={()=>{handleInputBlur("email")}}
             onChange={(event)=>{handleInputChange("email",event.target.value)}} 
             value={enteredData.email} 
           />
+        <div className="control-error">
+          {emailIsInvalid && <p>Please enter Valid Email Address</p>}
+        </div>
         </div>
 
         <div className="control no-margin">
