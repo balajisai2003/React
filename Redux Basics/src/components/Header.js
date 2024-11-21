177dc51ec53deb1a9ca9a0220ae8971d7f33356d
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-21 19:04:38 +0530

    Implement authentication functionality in Auth and Header components; integrate Redux actions for login and logout.

--- Redux Basics/src/components/Header.js
+++ Redux Basics/src/components/Header.js
@@ -1,11 +1,19 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/storeIndex';
 
 const Header = () => {
+
+  const auth = useSelector(({auth}) => auth.isAuthenticated);
+  const dispatch = useDispatch();
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
+          {auth && 
+          <>
           <li>
             <a href='/'>My Products</a>
           </li>
@@ -13,8 +21,16 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={()=>{dispatch(authActions.logout())}}>Logout</button>
           </li>
+          </>
+          }
+          {
+          !auth &&
+          <li>
+            <button onClick={()=>{dispatch(authActions.login())}}>Login</button>
+          </li>
+          }
         </ul>
       </nav>
     </header>
