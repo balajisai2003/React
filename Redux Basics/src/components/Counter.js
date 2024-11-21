d16038746448f5ae6b9677759ee34aa0bdd4fdaa
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-21 14:55:24 +0530

    Implement Redux store and counter functionality; update Counter component with increment, decrement, and increase by 5 features; add toggle functionality for counter visibility.

--- Redux Basics/src/components/Counter.js
+++ Redux Basics/src/components/Counter.js
@@ -1,12 +1,30 @@
+
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
+  const toggle = useSelector(state => state.showToggle);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
+  
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={()=>dispatch({type:"increment"})}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={()=>dispatch({type:"decrement"})}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
