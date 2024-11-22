fb07fdc6778601299f1e9961334252ae521e07f4
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-22 09:57:40 +0530

    Add UserProfile component and update App component for conditional rendering based on authentication status

--- Redux Basics/src/App.js
+++ Redux Basics/src/App.js
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import Header from './components/Header';
 import Auth from './components/Auth';
 import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 function App() {
 
   const auth = useSelector(({auth}) => auth.isAuthenticated);
@@ -10,7 +11,7 @@ function App() {
   return (
     <Fragment>
       <Header/>
-      {!auth && <Auth />}
+      {!auth ? <Auth />:<UserProfile />}
       <Counter />
     </Fragment>
   );
