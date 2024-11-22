cea4cf0c91b871b6261109f5adeaaa6e43fe16e5
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-22 10:52:57 +0530

    Wrap App component with Redux Provider to enable store access throughout the application

--- Redux-Adv/src/index.js
+++ Redux-Adv/src/index.js
@@ -7,5 +7,4 @@ import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 root.render(<Provider store={store}><App /></Provider>);
