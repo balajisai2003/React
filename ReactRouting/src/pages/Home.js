330756fe11a190dc5337fbd35c2c34aa9e698d74
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-25 09:35:40 +0530

    Add ProductsPage component and update routing in App component; enhance HomePage with navigation link to Products

--- ReactRouting/src/pages/Home.js
+++ ReactRouting/src/pages/Home.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return <div>Home Page</div>;
+    return <>
+            <div>Home Page</div>
+            <p>
+                Go to <Link to="/products">Products</Link>
+            </p>
+            </>;
 }
