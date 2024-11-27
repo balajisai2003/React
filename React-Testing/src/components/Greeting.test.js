572080ff65966d9b384de9285c6b1edc07323b9b
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-27 16:25:19 +0530

    Add unit tests for Greeting component: implement test to verify rendering of "Hello World" text.

---
+++ React-Testing/src/components/Greeting.test.js
@@ -0,0 +1,15 @@
+import {render, screen} from '@testing-library/react';
+import Greeting from './Greeting';
+
+describe('Greeting Component', ()=>{
+    test('renders Hello World as a text', () => {
+        //Arrange
+        render(<Greeting />);
+        //Act
+        const helloWorldElement = screen.getByText('Hello World', {exact: false});
+        //Assert
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+});
+
+
