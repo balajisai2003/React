42a07300c3d8ba1d79e760019e185cb73919eeb8
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-27 11:57:29 +0530

    Add event fetching and deletion functionality: implement useQuery for fetching event details and useMutation for deleting events in EventDetails component, along with error handling and loading states.

--- React-Query/src/components/Events/EventDetails.jsx
+++ React-Query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,70 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from 'react-router-dom';
+import { useQuery, useMutation  } from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
+import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const {data, isLoading, isError, error } = useQuery({
+    queryKey: ['event', params.id],
+    queryFn: ({signal})=> fetchEvent({signal,id:params.id}),
+  })
+
+  const {mutate} = useMutation({
+    mutationFn : deleteEvent,
+    onSuccess:()=>{
+      queryClient.invalidateQueries({ queryKey:['events'], refetchType: 'none'});
+      navigate('/events');
+    }
+  })
+
+  function handleDelete(){
+    mutate({id:params.id});
+  }
+
+  let content = <p>no content</p>
+  if(isLoading){
+    content = <div id='events-details-content' className='center'>
+                  <p>Loading...</p> 
+              </div>  
+  }
+  if(isError){
+    content = <div id='events-details-content' className='center'>
+                  <ErrorBlock title='Failed to load event' message={error.info?.message || 'Failed to load event'} />
+              </div>  
+  }
+  if(data){
+
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+
+    content = <>
+                <header>
+                  <h1>{data.title}</h1>
+                  <nav>
+                    <button onClick={handleDelete}>Delete</button>
+                    <Link to="edit">Edit</Link>
+                  </nav>
+                </header>
+                <div id="event-details-content">
+                  <img src={"http://localhost:3000/"+data.image} alt="" />
+                  <div id="event-details-info">
+                    <div>
+                      <p id="event-details-location">{data.location}</p>
+                      <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate +' @ '+data.time}</time>
+                    </div>
+                    <p id="event-details-description">{data.description}</p>
+                  </div>
+                </div>
+              </>
+  }
+  
   return (
     <>
       <Outlet />
@@ -12,23 +74,7 @@ export default function EventDetails() {
         </Link>
       </Header>
       <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+        {content}
       </article>
     </>
   );
