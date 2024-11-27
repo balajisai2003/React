2c4c6052532d0704f80cefa22b4c111b6e3e5834
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-27 12:39:53 +0530

    Add event editing functionality: implement event fetching and updating in EditEvent component, manage loading and error states, and enhance form submission handling.

--- React-Query/src/components/Events/EditEvent.jsx
+++ React-Query/src/components/Events/EditEvent.jsx
@@ -1,20 +1,52 @@
-import { Link, useNavigate } from 'react-router-dom';
-
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { fetchEvent, updateEvent } from '../../util/http.js';
 
 export default function EditEvent() {
+  const params = useParams();
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
 
-  function handleClose() {
-    navigate('../');
+  const {data, isLoading, isError, error} = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+  });
+
+  const {mutate} = useMutation({
+    mutationFn: updateEvent,
+
+  });
+
+  
+  
+  
+  
+  let content = <p className='centre'>No content</p>;
+  if (isLoading) {
+    content = <div className='centre'><LoadingIndicator/></div>;
   }
-
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  if (isError) {
+    content = (
+      <div className="center">
+        <ErrorBlock
+          title="Failed to load event"
+          message={error.info?.message || 'Failed to load event'}
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +54,21 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    );
+  }
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate('../');
+  }
+
+  function handleClose() {
+    navigate('../');
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
