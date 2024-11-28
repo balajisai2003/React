bd4e41269349acd3ba2bf469a88f5403ead0f343
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-28 12:53:32 +0530

    Implement state management for todos: add addTodoHandler to manage new todos in App component and pass handler to NewTodo component.

--- ReactTypeScript/my-app/src/App.tsx
+++ ReactTypeScript/my-app/src/App.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
+
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo ("Learn React"), new Todo ("Learn TypeScript")];
 
+  const [todos, setTodos] = useState<Todo[]>([new Todo ("Learn React"), new Todo ("Learn TypeScript")]);
+
+  const addTodoHandler=(text:string)=>{
+    const newTodo = new Todo (text);
+    setTodos((prevTodos: Todo[])=>{
+      return [...prevTodos, newTodo];
+    })
+  }
   return (
     <div>
-      <NewTodo/>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
