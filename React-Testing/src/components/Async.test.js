7a7b3a46cb1c317e25950693043607e32bcc0887
BalajiSai2003 <balajisai2003@outlook.com>
2024-11-28 09:56:18 +0530

    Add unit tests for Async component: implement test to verify rendering of posts on successful fetch.

---
+++ React-Testing/src/components/Async.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+describe('Async Component', ()=>{
+    test('renders posts if request is successful', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First Post'}]
+        });
+        //Arrange
+        render(<Async />);
+        //Act
+        //Assert
+        const listItemElements = await screen.findAllByRole('listitem');
+        expect(listItemElements).not.toHaveLength(0);
+    })
+});
